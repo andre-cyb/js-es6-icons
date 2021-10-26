bd04b3a7a6e4c3dc6fe53d03f5f34a5239a5d04d
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-26 14:50:56 +0200

    Milestone 2: icone colorate

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 console.log(arrayIcone);
 
+
 const container = document.querySelector(".card_container");
+
+//oggetto con colori per tipo di icona
+
+
+let coloriIcone = { animal: "blue", vegetable: "orange", user: "violet" };
 
 for (let i = 0; i < arrayIcone.length; i++) {
     let icona = arrayIcone[i];
     let { name, prefix, type, family } = icona;
 
+    let colori = coloriIcone[type];
     let card = `<div class="card my_col_5 align-items-center pt-2 " style="">
-                    <i class="${family} ${prefix}${name}"></i>
+                    <i class="${family} ${prefix}${name}" style="color:${colori}"></i>
                     <div class="card-body">${name}</div>
                 </div>`;
-    container.innerHTML += card;
 
+
+
+    container.innerHTML += card;
 }
